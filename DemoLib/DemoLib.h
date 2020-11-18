f1429518b90d526300b534c40c3fe8b747ddbf4e
shekharvishwa <shekhar.vishwakarma06@gmail.com>
2020-11-18 09:02:10 +0530

    Initial Project setup

---
+++ DemoLib/DemoLib.h
@@ -0,0 +1,19 @@
+//
+//  DemoLib.h
+//  DemoLib
+//
+//  Created by Shekhar Vishwakarma on 18/11/20.
+//  Copyright © 2020 Zensar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoLib.
+FOUNDATION_EXPORT double DemoLibVersionNumber;
+
+//! Project version string for DemoLib.
+FOUNDATION_EXPORT const unsigned char DemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoLib/PublicHeader.h>
+
+
---
+++ DemoLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoLibExample
+//
+//  Created by Shekhar Vishwakarma on 18/11/20.
+//  Copyright © 2020 Zensar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
