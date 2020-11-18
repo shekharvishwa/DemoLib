34dbf61361b6bc456f324bd03452d39d82acb5c2
shekharvishwa <shekhar.vishwakarma06@gmail.com>
2020-11-18 09:20:50 +0530

    Added test cases

---
+++ DemoLib/DemoLib.swift
@@ -0,0 +1,24 @@
+//
+//  DemoLib.swift
+//  DemoLib
+//
+//  Created by Shekhar Vishwakarma on 18/11/20.
+//  Copyright © 2020 Zensar. All rights reserved.
+//
+
+import Foundation
+
+
+public final class DemoLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- DemoLibTests/DemoLibTests.swift
+++ DemoLibTests/DemoLibTests.swift
@@ -11,6 +11,14 @@ import XCTest
 
 class DemoLibTests: XCTestCase {
 
+    
+    var demoLib: DemoLib!
+    
+    override func setUp() {
+        demoLib = DemoLib()
+    }
+
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,6 +27,15 @@ class DemoLibTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    
+    func testAdd() {
+        XCTAssertEqual(demoLib.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(demoLib.sub(a: 2, b: 1), 1)
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
